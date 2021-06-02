6a82176f37b3a0c8ac51fe1144e91fc58e320745
Rabz92 <rabbiailyas92@gmail.com>
2021-06-02 14:06:46 +0200

    Badge added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import WarningSign from './components/WarningSign';
+import MyBadge from './components/MyBadge'
 
 function App() {
   return (
     <div>
       <WarningSign title="bookstore"/>
+      <MyBadge text="Badge" color="danger"/>
     </div>
   );
 }
---
+++ src/components/MyBadge.jsx
@@ -0,0 +1,8 @@
+import {Badge} from 'react-bootstrap'
+
+const MyBadge =(props)=>(
+<Badge variant={props.color}>{props.text}</Badge>
+)
+
+export default MyBadge
+
