a1b56096441a806e6d95e57e22b3459f10365c7c
Rabz92 <rabbiailyas92@gmail.com>
2021-06-02 16:46:47 +0200

    singleBook class

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,27 +1,56 @@
 
 import {Container, Row, Card, Button} from 'react-bootstrap'
+import {Component} from 'react'
 
-const SingleBook =(props) => (
+class SingleBook extends Component{
+
+    state = {
+
+        selected:false
+    }
+
+    render(){
+        return(
+            <Container>
+            <Row className="justify-content-center mt-3"> 
+             
+             {this.props["name"].map ((book,index) => ( 
+                // this.setState={selected: book}
+                <Card 
+                id={"book.asin"}
+                className="mt-3 mx-3" 
+                key={index}  
+                style={{ width: '14rem'}}>
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <Card.Text>
+                    <p>{book.category}</p> 
+                    <span onMouseOver={(e)=>{
+                        e.target.innerText = 'Discount 50%'
+                    }}
+                    onMouseOut={(e)=>{
+                        e.target.innerText = book.price
+                    }}>{book.price}</span>
+                    </Card.Text>
+                    <Button onClick={(e)=>{
+                    e.target.innerText = 'Bought'
+                    return this.setState({selected:true}) 
+             }   
+               } 
+               variant="primary">Buy</Button>
+                </Card.Body>
+                </Card>))
+                }
+                </Row>
+                </Container>
+        )
+    }
+ 
+}
+
         
-<Container>
-   <Row className="justify-content-center mt-3"> 
-    
-    {props["name"].map ((book,index) => ( 
-       // this.setState={selected: book}
-       <Card className="mt-3 mx-3" key={index}  style={{ width: '14rem'}}>
-       <Card.Img variant="top" src={book.img} />
-       <Card.Body>
-           <Card.Title>{book.title}</Card.Title>
-           <Card.Text>
-           <p>{book.category}</p> 
-           <span>{book.price}</span>
-           </Card.Text>
-           <Button variant="primary">Buy</Button>
-       </Card.Body>
-       </Card>))
-       }
-       </Row>
-       </Container>
-)
+
+
 
 export default SingleBook
