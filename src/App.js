04852615b28e208d2d9610a7301f93db59cf6a97
Gamze Güneri <62291935+pinacotheque@users.noreply.github.com>
2021-06-02 15:25:57 +0300

    ex.3 books added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import WarningSign from './components/WarningSign';
 import MyBadge from './components/MyBadge'
+import books from "./data/fantasy.json"
+import SingleBook from './components/SingleBook'
+
 
 function App() {
   return (
     <div>
       <WarningSign title="bookstore"/>
       <MyBadge text="Badge" color="danger"/>
+      <SingleBook name={books}/>
     </div>
   );
 }
---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,26 @@
+
+import {Container, Row, Card, Button} from 'react-bootstrap'
+
+const SingleBook =(props) => (
+        
+<Container>
+   <Row className="justify-content-center mt-3"> 
+    
+    {props["name"].map ((book,index) => ( 
+       // this.setState={selected: book}
+       <Card className="mt-3 mx-3" key={index}  style={{ width: '14rem'}}>
+       <Card.Img variant="top" src={book.img} />
+       <Card.Body>
+           <Card.Title>{book.title}</Card.Title>
+           <Card.Text>
+           Some quick 
+           </Card.Text>
+           <Button variant="primary">Buy</Button>
+       </Card.Body>
+       </Card>))
+       }
+       </Row>
+       </Container>
+)
+
+export default SingleBook
