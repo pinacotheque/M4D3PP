4054472d360150b1b3c7d80c82b2f46375d4cb3d
Gamze Güneri <62291935+pinacotheque@users.noreply.github.com>
2021-06-10 17:47:40 +0300

    working on controlled form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,13 +1,42 @@
-import {Button, Form} from 'react-bootstrap'
+import {Button, Form,Col} from 'react-bootstrap'
+import {useState} from 'react'
 
 
 
 const Registration = () => {
-return(
 
-        <Form>
+    const [name, setName] = useState('')
+    const [surname, setSurname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+   const handleSubmit =(event) =>{
+        alert('A name was submitted: ');
+        event.preventDefault();
+      }
+    
+return(
+        <Col className={'w-100'}>
+        <Form className={'mt-5 mx-5'} onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+        <Form.Label >Name:</Form.Label>
+        
+        <Form.Control type="text" placeholder="Normal text" />
+        <Form.Text className="text-muted">
+        At least 2 characters
+    </Form.Text>
+        </Form.Group>
+        
+        <Form.Group controlId="formBasicEmail">
+        <Form.Label>Surname:</Form.Label>
+        <Form.Control type="text" placeholder="Normal text" />
+        <Form.Text className="text-muted">
+        At least 3 characters
+    </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address:</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
             We'll never share your email with anyone else.
@@ -15,16 +44,28 @@ return(
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label>Password:</Form.Label>
             <Form.Control type="password" placeholder="Password" />
+            <Form.Text className="text-muted">
+            Should contain at least 8 chars, 1 digit, 1 letter
+    </Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password confirmation:</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            
         </Form.Group>
         
         <Button variant="primary" type="submit">
             Submit
         </Button>
         </Form>
+        </Col>
 )
 
 }
 
 export default Registration
+
+
+
